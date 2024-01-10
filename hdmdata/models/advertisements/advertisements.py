633a2b2355a22cdbf9e279e70f80560de677b8e9
Danilo Paes <danilo.paes@pm.me>
2024-01-10 21:46:37 +0100

    fix #4, fix #8: implements table constraints and db schema
    Implements constraints to rent_office and advertisements models and also the
    schema `homedigger`
    
    Signed-off-by: Danilo Paes <danilo.paes@pm.me>

--- hdmdata/models/advertisements/advertisements.py
+++ hdmdata/models/advertisements/advertisements.py
@@ -1,11 +1,16 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from hdmdata.database._base_model import BaseModel
 
 
 class Advertisements(BaseModel):
-    __tablename__ = 'advertisements'
+    __table_args__ = (
+        UniqueConstraint(
+            "title", "link", "price", "parking", "rooms", "size", "rent_office_id"
+        ),
+        {"schema": "homedigger"},
+    )
 
     title = Column(String(length=200), nullable=False)
     link = Column(String(length=200), nullable=False)
@@ -13,7 +18,7 @@ class Advertisements(BaseModel):
     parking = Column(Boolean, default=False)
     rooms = Column(Integer, nullable=False)
     size = Column(Integer, nullable=False)
-    
+
     rent_office_id = Column(UUID, ForeignKey('rent_office.id'))
     rent_office = relationship(
         "RentOffice",
--- hdmdata/models/advertisements/rent_office.py
+++ hdmdata/models/advertisements/rent_office.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, UniqueConstraint
 from hdmdata.database._base_model import BaseModel
 
 
 class RentOffice(BaseModel):
-    __tablename__ = 'rent_office'
+    __table_args__ = (UniqueConstraint("title", "link"), {"schema": "homedigger"})
 
     title = Column(String(length=200), nullable=False)
     link = Column(String(length=100), nullable=False)
-    
+
     advertisements = relationship(
        "Advertisements", 
         lazy=True,
